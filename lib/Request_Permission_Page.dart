bdea8b0d298233eac72f89517195341fe6f42c0e
Adrian González <adrigondo.profesional@gmail.com>
2023-01-04 12:46:01 -0600

    Temporal

--- lib/Request_Permission_Page.dart
+++ lib/Request_Permission_Page.dart
@@ -1,9 +1,9 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mapavirtual/Request_Permission_Controller.dart';
 import 'package:mapavirtual/routes.dart';
 import 'package:permission_handler/permission_handler.dart';
+
 class RequestPermissionPage extends StatefulWidget {
   const RequestPermissionPage({Key? key}) : super(key: key);
 
@@ -11,55 +11,68 @@ class RequestPermissionPage extends StatefulWidget {
   _RequestPermissionPageState createState() => _RequestPermissionPageState();
 }
 
-class _RequestPermissionPageState extends State<RequestPermissionPage> with WidgetsBindingObserver{
+class _RequestPermissionPageState extends State<RequestPermissionPage>
+    with WidgetsBindingObserver {
   final _controller = RequestPermissionController(Permission.locationWhenInUse);
   late StreamSubscription _subscription;
   bool _fromSettings = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
     _subscription = _controller.onStatusChanged.listen(
-            (status) {
-              switch (status){
-                case PermissionStatus.granted:
-                  _goToHome();
-                  break;
-                case PermissionStatus.permanentlyDenied:
-                  showDialog(
-                      context: context,
-                      builder: (_)=>AlertDialog(
-                        title: const Text("INFO"),
-                        content: const Text("Necesita otorgar permisos."),
-                        actions: [
-                          TextButton(
-                              onPressed: () async{
-                                Navigator.pop(context);
-                                _fromSettings = await openAppSettings();
-                              },
-                              child: const Text("Ir a ajustes"),
-                          ),
-                          TextButton(
-                              onPressed: (){
-                                Navigator.pop(context);
-                              },
-                              child: const Text("Cancelar"),
-                          ),
-                        ],
-                      ),
-                  );
-                  break;
-              }
-        },
+      (status) {
+        switch (status) {
+          case PermissionStatus.granted:
+            _goToHome();
+            break;
+          case PermissionStatus.permanentlyDenied:
+            showDialog(
+              context: context,
+              builder: (_) => AlertDialog(
+                title: const Text("INFO"),
+                content: const Text("Necesita otorgar permisos."),
+                actions: [
+                  TextButton(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      _fromSettings = await openAppSettings();
+                    },
+                    child: const Text("Ir a ajustes"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Cancelar"),
+                  ),
+                ],
+              ),
+            );
+            break;
+          case PermissionStatus.undetermined:
+            // TODO: Handle this case.
+            break;
+          case PermissionStatus.denied:
+            // TODO: Handle this case.
+            break;
+          case PermissionStatus.restricted:
+            // TODO: Handle this case.
+            break;
+          case PermissionStatus.limited:
+            // TODO: Handle this case.
+            break;
+        }
+      },
     );
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) async {
-    if(state == AppLifecycleState.resumed && _fromSettings){
+    if (state == AppLifecycleState.resumed && _fromSettings) {
       final status = await _controller.check();
-      if(status == PermissionStatus.granted){
+      if (status == PermissionStatus.granted) {
         _goToHome();
       }
     }
@@ -67,19 +80,21 @@ class _RequestPermissionPageState extends State<RequestPermissionPage> with Widg
   }
 
   @override
-  void dispose(){
+  void dispose() {
     WidgetsBinding.instance.removeObserver(this);
     _subscription.cancel();
     _controller.dispose();
     super.dispose();
   }
-  void _goToHome(){
+
+  void _goToHome() {
     Navigator.pushReplacementNamed(context, Routes.HOME);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(
+      body: SafeArea(
         child: Container(
           width: double.infinity,
           height: double.infinity,
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,8 @@
-
+// ignore_for_file: constant_identifier_names
 
 abstract class Routes {
-  static const SPLASH ='/';
-  static const HOME ='/home';
-  static const PERMISSIONS ='/permissions';
-  static const ARCORE ='/RA';
+  static const SPLASH = '/';
+  static const HOME = '/home';
+  static const PERMISSIONS = '/permissions';
+  static const ARCORE = '/RA';
 }
